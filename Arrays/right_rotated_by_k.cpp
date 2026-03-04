a0f643dc0b67c984e9b1807a42d6915a41436a4e
NexusAman <amankumarkeshri1@gmail.com>
2026-03-04 22:58:28 +0530

    solved rightRotatedByK problem 189

---
+++ Arrays/right_rotated_by_k.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Brute Force
+// void rotate(vector<int> &nums, int k) {
+//   int n = nums.size();
+//   vector<int> temp;
+//   for (int i = n - k; i < n; i++) {
+//     temp.push_back(nums[i]);
+//   }
+
+//   for (int i = 0; i < n - k; i++) {
+//     temp.push_back(nums[i]);
+//   }
+
+//   for (auto &val : temp) {
+//     cout << val << " ";
+//   }
+// }
+
+void rotate(vector<int> &nums, int k) {
+  reverse(nums.begin(), nums.end());
+  reverse(nums.begin(), nums.begin() + k);
+  reverse(nums.begin() + k, nums.end());
+}
+
+int main() {
+  vector<int> nums = {1, 2, 3, 4, 5};
+  rotate(nums, 2);
+  for (auto &val : nums) {
+    cout << val << " ";
+  }
+  return 0;
+}
