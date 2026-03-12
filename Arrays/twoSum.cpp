02b071f747d3a1a66897dfa5d732ef75375c2cff
NexusAman <amankumarkeshri1@gmail.com>
2026-03-12 23:28:14 +0530

    solved optimal approach of twoSum problem

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 vector<int> twoSum(vector<int> &nums, int target)
 {
+    /*
+    //Brute Force
     for (int i = 0; i < nums.size(); i++)
     {
         for (int j = i + 1; j < nums.size(); j++)
@@ -11,6 +13,58 @@ vector<int> twoSum(vector<int> &nums, int target)
                 return {i, j};
             }
         }
+    }
+    return {-1, -1};
+    */
+
+    /*
+    // Better Approach
+    int n = nums.size();
+    vector<pair<int, int>> originalIndex;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        originalIndex.push_back({nums[i], i});
+    }
+
+    sort(originalIndex.begin(), originalIndex.end());
+
+    int i = 0, j = n - 1;
+
+    while (i < j)
+    {
+        long long sum = originalIndex[i].first + originalIndex[j].first;
+
+        if (sum == target)
+        {
+            return {originalIndex[i].second, originalIndex[j].second};
+        }
+        else if (sum > target)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return {-1, -1};
+    */
+
+    // Optimal Approach
+    unordered_map<int, int> sum;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        int complement = target - nums[i];
+
+        if (sum.find(complement) != sum.end())
+        {
+            return {sum[complement], i};
+        }
+
+        sum[nums[i]] = i;
     }
     return {-1, -1};
 }
