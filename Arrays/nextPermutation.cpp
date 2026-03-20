d7412b2e96df9a947343f0c35a494aa684a35052
NexusAman <amankumarkeshri1@gmail.com>
2026-03-20 21:37:07 +0530

    solved nextPermutation optimal approach

--- Arrays/nextPermutation.cpp
+++ Arrays/nextPermutation.cpp
@@ -6,32 +6,37 @@ void nextPermutation(vector<int> &nums)
     //  next_permutation(nums.begin(), nums.end());
 
     // Optimal Approach
-    // first find the pivot element
     int n = nums.size();
     int pivot = -1;
     int swapIndex = -1;
 
+    // first find the pivot element
     for (int i = n - 1; i > 0; i--)
     {
         if (nums[i - 1] < nums[i])
         {
             pivot = i - 1;
-            cout << pivot << endl;
-        }
-    }
-
-    // find the rightmost element greater than pivot (just greater)
-    for (int i = n - 1; i > 0; i--)
-    {
-        if (nums[i] > pivot)
-        {
-            swapIndex = i;
-            cout << swapIndex << endl;
             break;
         }
     }
 
-    swap(nums[pivot], nums[swapIndex]);            // swap with pivot element
+    // if array has no next permutation
+    if (pivot == -1)
+    {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    // find the rightmost element greater than pivot (just greater)
+    for (int i = n - 1; i > pivot; i--)
+    {
+        if (nums[i] > pivot)
+        {
+            swap(nums[i], nums[pivot]); // swap with pivot element
+            break;
+        }
+    }
+
     reverse(nums.begin() + pivot + 1, nums.end()); // reverse the portion after pivot
 }
 int main()
