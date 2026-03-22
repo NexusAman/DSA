10fc075f64ca78d20d647022eaf9bd4bbab55e73
NexusAman <amankumarkeshri1@gmail.com>
2026-03-22 09:42:07 +0530

    solved longestConsecutiveSequence problem using brute and optimal approach

---
+++ Arrays/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void hello(string name)
+{
+    cout << "good night " + name + "!";
+    // return;
+}
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    hello("Aman");
+    return 0;
+}
--- Arrays/longestConsecutiveSequence.cpp
+++ Arrays/longestConsecutiveSequence.cpp
@@ -1,7 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool linearSearch(int element, vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == element)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int longestConsecutive(vector<int> &nums)
 {
+    /*
+    // Brute Force
+    if(nums.empty()) return 0;
+    int maxSequence = 1;
+    int element;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        element = nums[i];
+        int count = 1;
+
+        while (linearSearch(element + 1, nums))
+        {
+            element += 1;
+            count++;
+        }
+        maxSequence = max(maxSequence, count);
+    }
+
+    return maxSequence;
+    */
+
+    /*
     // Better Approach
     int n = nums.size();
     if (n == 0)
@@ -31,13 +67,38 @@ int longestConsecutive(vector<int> &nums)
 
         maxSequence = max(maxSequence, count);
     }
+    return maxSequence;
+    */
+
+    // Optimal Approach
+    if (nums.empty())
+        return 0;
+    unordered_set<int> st(nums.begin(), nums.end()); // O(n)
+    int maxSequence = 1;
+
+    for (const auto &num : st) // O(n)
+    {
+        // inner loop O(n) because it skips element if it is not the first element of the sequence
+        if (st.find(num - 1) == st.end())
+        {
+            int current = num;
+            int count = 1;
+
+            while (st.find(current + 1) != st.end()) // O(n)
+            {
+                count++;
+                current++;
+            }
+            maxSequence = max(maxSequence, count);
+        }
+    }
 
     return maxSequence;
 }
 
 int main()
 {
-    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    vector<int> nums = {100, 4, 200, 1, 3, 2, 5};
     cout << longestConsecutive(nums);
     return 0;
 }
