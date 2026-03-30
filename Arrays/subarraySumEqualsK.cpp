03fad9106e83e77202d87a1415c8db78837a012e
NexusAman <amankumarkeshri1@gmail.com>
2026-03-30 19:43:39 +0530

    solved subarraySumEqualsK problem by using optimal approach

--- Arrays/subarraySumEqualsK.cpp
+++ Arrays/subarraySumEqualsK.cpp
@@ -44,6 +44,24 @@ int subarraySum(vector<int> &nums, int k)
     */
 
     // Optimal Approach
+    unordered_map<int, int> mpp;
+
+    int prefixSum = 0;
+    int count = 0;
+    mpp[0] = 1;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        prefixSum += nums[i]; // 1
+
+        if (mpp.find(prefixSum - k) != mpp.end())
+        {
+            count += mpp[prefixSum - k];
+        }
+        mpp[prefixSum]++;
+    }
+
+    return count;
 }
 int main()
 {
