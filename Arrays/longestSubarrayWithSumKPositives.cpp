7c411a71c3b1d925e7636d638253169e26d81adc
NexusAman <amankumarkeshri1@gmail.com>
2026-04-01 21:00:24 +0530

    solved longestSubarrayWithSumKPositives using brute force approach

---
+++ Arrays/longestSubarrayWithSumKPositives.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int longestSubarray(vector<int> &nums, int k)
+{
+    int maxLen = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int sum = 0;
+        for (int j = i; j < nums.size(); j++)
+        {
+            sum += nums[j];
+
+            if (sum == k)
+            {
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+    }
+    return maxLen;
+}
+int main()
+{
+    vector<int> nums = {10, 5, 2, 7, 1, 9};
+    int k = 15;
+    int longestLength = longestSubarray(nums, k);
+    cout << "Longest subarray length : " << longestLength;
+    return 0;
+}
