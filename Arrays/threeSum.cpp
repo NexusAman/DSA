2cd07851bcca9b9ff523dbbb247bf520c1e99a33
NexusAman <amankumarkeshri1@gmail.com>
2026-04-07 22:14:53 +0530

    solved threeSum problem using better approach

--- Arrays/threeSum.cpp
+++ Arrays/threeSum.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 vector<vector<int>> threeSum(vector<int> &nums)
 {
+    /*
+    //Brute Force
     set<vector<int>> st;
     for (int i = 0; i < nums.size(); i++)
     {
@@ -16,6 +18,29 @@ vector<vector<int>> threeSum(vector<int> &nums)
                     st.insert(temp);
                 }
             }
+        }
+    }
+
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+    */
+
+    // Better Approach
+    set<vector<int>> st;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        set<int> hashset;
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            int third = -(nums[i] + nums[j]);
+            if (hashset.find(third) != hashset.end())
+            {
+                vector<int> temp = {nums[i], nums[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(nums[j]);
         }
     }
 
