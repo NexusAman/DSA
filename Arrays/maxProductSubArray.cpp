06f04817498f672cd24440ecd5e27688ace12f42
NexusAman <amankumarkeshri1@gmail.com>
2026-04-20 19:22:54 +0530

    solved maxProductSubArray using optimal approach

--- Arrays/maxProductSubArray.cpp
+++ Arrays/maxProductSubArray.cpp
@@ -23,7 +23,7 @@ int maxProduct(vector<int> &nums)
     */
 
     /*
-    Better Approach
+    // Better Approach
     int n = nums.size();
     int maxProd = nums[0];
 
@@ -38,6 +38,27 @@ int maxProduct(vector<int> &nums)
     }
     return maxProd;
     */
+
+    // Optimal Approach
+    int n = nums.size();
+
+    int pre = 1, suff = 1;
+    int ans = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (pre == 0)
+            pre = 1;
+        if (suff == 0)
+            suff = 1;
+
+        pre *= nums[i];
+        suff *= nums[n - i - 1];
+
+        ans = max(ans, max(pre, suff));
+    }
+
+    return ans;
 }
 int main()
 {
