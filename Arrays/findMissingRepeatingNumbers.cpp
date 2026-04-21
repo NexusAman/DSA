944b732839c58ce45fe8bbaa06221fcee25cd82d
NexusAman <amankumarkeshri1@gmail.com>
2026-04-21 08:06:51 +0530

    solved findMissingRepeatingNumbers using brute force approach

--- Arrays/findMissingRepeatingNumbers.cpp
+++ Arrays/findMissingRepeatingNumbers.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 vector<int> findMissingRepeatingNumbers(vector<int> &nums)
 {
+    // Brute Force Approach
+    int n = nums.size();
+    int A = -1, B = -1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j] == i)
+            {
+                cnt++;
+            }
+        }
+        if (cnt == 2)
+        {
+            A = i;
+        }
+        else if (cnt == 0)
+        {
+            B = i;
+        }
+
+        if (A != -1 && B != -1)
+        {
+            break;
+        }
+    }
+    return {A, B};
+
+    /*
+    //By Self
     unordered_map<int, int> mpp;
     int A = -1;
     int B = -1;
@@ -29,6 +61,7 @@ vector<int> findMissingRepeatingNumbers(vector<int> &nums)
     B = totalSum - (arrSum - A);
 
     return {A, B};
+    */
 }
 int main()
 {
