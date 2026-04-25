5642ce496fb66a05ab2d78bfa12c0638d8d205a5
NexusAman <amankumarkeshri1@gmail.com>
2026-04-25 08:55:33 +0530

    solved longestSubarrayWithSumKPositives using better approach

--- Arrays/longestSubarrayWithSumKPositives.cpp
+++ Arrays/longestSubarrayWithSumKPositives.cpp
@@ -21,7 +21,30 @@ int longestSubarray(vector<int> &nums, int k)
     return maxLen;
     */
 
-    // Optimal Approach
+    // Better Approach
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        int rem = sum - k;
+
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+        preSumMap[sum] = i;
+    }
+    return maxLen;
 }
 int main()
 {
