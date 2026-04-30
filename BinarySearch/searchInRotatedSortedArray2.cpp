6a158fc0085a7235a7a958d5977dbff303655315
NexusAman <amankumarkeshri1@gmail.com>
2026-04-30 19:19:05 +0530

    solved searchInRotatedSortedArray2 using optimal approach

--- BinarySearch/searchInRotatedSortedArray2.cpp
+++ BinarySearch/searchInRotatedSortedArray2.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 bool search(vector<int> &nums, int target)
 {
+    /*
+    //Brute Force Approach
     int n = nums.size();
 
     for (int i = 0; i < n; i++)
@@ -12,11 +14,58 @@ bool search(vector<int> &nums, int target)
         }
     }
     return false;
+    */
+
+    // Optimal Approach
+    int n = nums.size();
+    int low = 0, high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+            return true;
+
+        // 🔥 HANDLE DUPLICATES
+        if (nums[low] == nums[mid] && nums[mid] == nums[high])
+        {
+            low++;
+            high--;
+        }
+
+        // Left sorted
+        else if (nums[low] <= nums[mid])
+        {
+            if (nums[low] <= target && target < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        // Right sorted
+        else
+        {
+            if (nums[mid] < target && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
 }
 int main()
 {
     vector<int> nums = {2, 5, 6, 0, 0, 1, 2};
-    int target = 0;
+    int target = 3;
     cout << search(nums, target);
     return 0;
 }
