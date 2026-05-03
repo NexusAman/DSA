65c52f999110d2b53fede70006bf2e49820cbd9e
NexusAman <amankumarkeshri1@gmail.com>
2026-05-03 07:41:04 +0530

    solved findKRotation using optimal approach

--- BinarySearch/findKRotation.cpp
+++ BinarySearch/findKRotation.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int findKRotation(vector<int> &nums)
 {
+    /*
+    // Brute Force Approach
+    int n = nums.size();
+    int min = INT_MAX;
+    int index = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < min)
+        {
+            min = nums[i];
+            index = i;
+        }
+    }
+    return index;
+    */
+
+    /*
+    //Better Approach
     int n = nums.size();
 
     for (int i = 0; i < n - 1; i++)
@@ -12,6 +31,38 @@ int findKRotation(vector<int> &nums)
         }
     }
     return 0;
+    */
+
+    // Optimal Approach
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    int ans = INT_MAX;
+    int index = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[low] <= nums[mid])
+        {
+            if (nums[low] < ans)
+            {
+                ans = nums[low];
+                index = low;
+            }
+            low = mid + 1;
+        }
+        else
+        {
+            if (nums[mid] < ans)
+            {
+                ans = nums[mid];
+                index = mid;
+            }
+            high = mid - 1;
+        }
+    }
+    return index;
 }
 int main()
 {
