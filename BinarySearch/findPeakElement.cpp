1539bec7053976e86398bf80f62b5137488f0aed
NexusAman <amankumarkeshri1@gmail.com>
2026-05-03 08:16:07 +0530

    solved findPeakElement using brute force approach

---
+++ BinarySearch/findPeakElement.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findPeakElement(vector<int> &nums)
+{
+    int n = nums.size();
+    int index = -1;
+    if (n == 1)
+        return 0;
+    if (nums[0] > nums[1])
+        return 0;
+    if (nums[n - 1] > nums[n - 2])
+        return n - 1;
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> nums = {1, 2, 1, 3, 5, 6, 4};
+    cout << findPeakElement(nums);
+    return 0;
+}
