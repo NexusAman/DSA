8f1922c9231335fadf41b5dcf433a64dff7d5b1f
NexusAman <amankumarkeshri1@gmail.com>
2026-05-04 17:49:58 +0530

    solved findPeakElement using optimal approach

--- BinarySearch/findPeakElement.cpp
+++ BinarySearch/findPeakElement.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int findPeakElement(vector<int> &nums)
 {
+    /*
+    //Brute Force Approach
     int n = nums.size();
     int index = -1;
     if (n == 1)
@@ -16,6 +18,43 @@ int findPeakElement(vector<int> &nums)
         if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
         {
             return i;
+        }
+    }
+    return -1;
+    */
+
+    // Optimal Approach
+    int n = nums.size();
+    if (n == 1)
+    {
+        return 0;
+    }
+    if (nums[0] > nums[1])
+    {
+        return 0;
+    }
+    if (nums[n - 1] > nums[n - 2])
+    {
+        return n - 1;
+    }
+
+    int low = 1, high = n - 2;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+        {
+            return mid;
+        }
+        else if (nums[mid + 1] > nums[mid])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
         }
     }
     return -1;
