33106fd888518c14c18d20fdf1ea304db529298d
NexusAman <amankumarkeshri1@gmail.com>
2026-05-09 13:21:29 +0530

    solved capactiyShipPackagesDDays using optimal approach

--- BinarySearch/BS On Answers/capactiyShipPackagesDDays.cpp
+++ BinarySearch/BS On Answers/capactiyShipPackagesDDays.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int shipWithinDays(vector<int> &weights, int days)
 {
+    /*
+    //Brute force approach
     int n = weights.size();
     long long totalweights = 0;
     int maxWeight = 0;
@@ -35,6 +37,51 @@ int shipWithinDays(vector<int> &weights, int days)
         }
     }
     return -1;
+    */
+
+    // Optimal approach
+    int n = weights.size();
+    long long totalweights = 0;
+    int maxWeight = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        totalweights += weights[i];
+        maxWeight = max(maxWeight, weights[i]);
+    }
+
+    int low = maxWeight, high = totalweights;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int dayUsed = 1;
+        long long currentLoad = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (currentLoad + weights[i] <= mid)
+            {
+                currentLoad += weights[i];
+            }
+            else
+            {
+                dayUsed++;
+                currentLoad = weights[i];
+            }
+        }
+        if (dayUsed <= days)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
 }
 int main()
 {
