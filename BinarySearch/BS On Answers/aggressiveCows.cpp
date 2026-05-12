47eedcfa6f93f878d396222cd63615be0bd0231d
NexusAman <amankumarkeshri1@gmail.com>
2026-05-12 22:38:16 +0530

    solved aggressiveCows problem using optimal approach

--- BinarySearch/BS On Answers/aggressiveCows.cpp
+++ BinarySearch/BS On Answers/aggressiveCows.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+//Brute force approach
 bool canWePlace(vector<int> &stalls, int k, int d)
 {
     int n = stalls.size();
@@ -39,6 +41,51 @@ int aggressiveCows(vector<int> &stalls, int k)
     }
     return (max - min);
 }
+*/
+bool canWePlace(vector<int> &stalls, int k, int dist)
+{
+    int n = stalls.size();
+    int cntCows = 1, lastCow = stalls[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if ((stalls[i] - lastCow) >= dist)
+        {
+            cntCows++;
+            lastCow = stalls[i];
+        }
+    }
+    if (cntCows >= k)
+    {
+        return true;
+    }
+    return false;
+}
+
+// Optimal approach
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+    int n = stalls.size();
+
+    int low = 1, high = stalls[n - 1] - stalls[0];
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (canWePlace(stalls, k, mid))
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
 int main()
 {
     vector<int> stalls = {1, 2, 4, 8, 9};
