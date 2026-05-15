f538e35308364c6175dde2e60cc2b428fe9ec546
NexusAman <amankumarkeshri1@gmail.com>
2026-05-15 20:33:43 +0530

    solved bookAllocationProblem using optimal approach

--- BinarySearch/BS On Answers/bookAllocationProblem.cpp
+++ BinarySearch/BS On Answers/bookAllocationProblem.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+//Brute force apporach
 int countStudents(vector<int> &nums, int pages)
 {
     int n = nums.size();
@@ -39,6 +41,57 @@ int findPages(vector<int> &nums, int m)
         }
     }
     return -1;
+}
+*/
+
+// Optimal Approach
+int countStudents(vector<int> &nums, int pages)
+{
+    int n = nums.size();
+    int students = 1;
+    int pagesCount = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((pagesCount + nums[i]) <= pages)
+        {
+            pagesCount += nums[i];
+        }
+        else
+        {
+            students++;
+            pagesCount = nums[i];
+        }
+    }
+    return students;
+}
+int findPages(vector<int> &nums, int m)
+{
+    int n = nums.size();
+    if (m > n)
+    {
+        return -1;
+    }
+
+    int low = *max_element(nums.begin(), nums.end());
+    long long high = accumulate(nums.begin(), nums.end(), 0);
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (countStudents(nums, mid) <= m)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
 }
 int main()
 {
