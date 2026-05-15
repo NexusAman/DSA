e32f5f45deabcc782068e75f6c2f134f8a90195d
NexusAman <amankumarkeshri1@gmail.com>
2026-05-15 22:38:58 +0530

    solved splitArrayLargestSum using optimal approach

--- BinarySearch/BS On Answers/splitArrayLargestSum.cpp
+++ BinarySearch/BS On Answers/splitArrayLargestSum.cpp
@@ -38,6 +38,52 @@ int splitArray(vector<int> &nums, int k)
     return -1;
 }
 */
+
+// Optimal Approach
+int countPartitions(vector<int> &nums, int maxSum)
+{
+    int n = nums.size();
+    int partitions = 1;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((sum + nums[i]) <= maxSum)
+        {
+            sum += nums[i];
+        }
+        else
+        {
+            partitions++;
+            sum = nums[i];
+        }
+    }
+    return partitions;
+}
+int splitArray(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int low = *max_element(nums.begin(), nums.end());
+    long long high = accumulate(nums.begin(), nums.end(), 0);
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (countPartitions(nums, mid) <= k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
     vector<int> nums = {7, 2, 5, 10, 8};
