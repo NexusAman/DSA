1440f5a7a8afea6325d560b2e8a4d128171bdc37
NexusAman <amankumarkeshri1@gmail.com>
2026-05-16 20:00:40 +0530

    solved paintersPartitionProblem using optimal approach

--- BinarySearch/BS On Answers/paintersPartitionProblem.cpp
+++ BinarySearch/BS On Answers/paintersPartitionProblem.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+//Brute force approach
 int countPainters(vector<int> arr, int maxTime)
 {
     int n = arr.size();
@@ -35,6 +37,55 @@ int minTime(vector<int> &arr, int k)
     }
     return -1;
 }
+*/
+
+// Optimal Approach
+int countPainters(vector<int> arr, int maxTime)
+{
+    int n = arr.size();
+    int painters = 1;
+    int boardsPainted = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((boardsPainted + arr[i]) <= maxTime)
+        {
+            boardsPainted += arr[i];
+        }
+        else
+        {
+            painters++;
+            boardsPainted = arr[i];
+        }
+    }
+    return painters;
+}
+
+int minTime(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    int low = *max_element(arr.begin(), arr.end());
+    long long high = accumulate(arr.begin(), arr.end(), 0);
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (countPainters(arr, mid) <= k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
     vector<int> arr = {10, 20, 30, 40};
