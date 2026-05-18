6a2df1cc527d150e2d1e701373664d693874fc76
NexusAman <amankumarkeshri1@gmail.com>
2026-05-18 14:52:55 +0530

    medianOfTwoSortedArrays using better approach

--- BinarySearch/BS On Answers/medianOfTwoSortedArrays.cpp
+++ BinarySearch/BS On Answers/medianOfTwoSortedArrays.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+//Brute force approach
 vector<int> merge(vector<int> &nums1, vector<int> &nums2)
 {
     int m = nums1.size();
@@ -51,6 +53,83 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     }
     return median;
 }
+*/
+
+// Better approach
+double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+{
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+
+    int n = n1 + n2;
+
+    int ind1 = n / 2;
+    int ind2 = n / 2 - 1;
+
+    int ind1el = -1;
+    int ind2el = -1;
+
+    int cnt = 0;
+    int left = 0, right = 0;
+
+    while (left < n1 && right < n2)
+    {
+        int currentElement;
+
+        if (nums1[left] <= nums2[right])
+        {
+            currentElement = nums1[left];
+            left++;
+        }
+        else
+        {
+            currentElement = nums2[right];
+            right++;
+        }
+
+        if (cnt == ind1)
+            ind1el = currentElement;
+
+        if (cnt == ind2)
+            ind2el = currentElement;
+
+        cnt++;
+    }
+
+    while (left < n1)
+    {
+        int currentElement = nums1[left];
+
+        if (cnt == ind1)
+            ind1el = currentElement;
+
+        if (cnt == ind2)
+            ind2el = currentElement;
+
+        cnt++;
+        left++;
+    }
+
+    while (right < n2)
+    {
+        int currentElement = nums2[right];
+
+        if (cnt == ind1)
+            ind1el = currentElement;
+
+        if (cnt == ind2)
+            ind2el = currentElement;
+
+        cnt++;
+        right++;
+    }
+
+    if (n % 2 == 1)
+        return ind1el;
+
+    return (double)(ind1el + ind2el) / 2;
+}
+
 int main()
 {
     vector<int> nums1 = {1, 2};
