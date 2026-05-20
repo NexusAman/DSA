cf1d1e8e5b5be44cf892f6532c5d6b410c43743b
NexusAman <amankumarkeshri1@gmail.com>
2026-05-20 22:40:12 +0530

    solved minimizeMaxDistanceToGasStations usingnbetter approach

--- BinarySearch/BS On Answers/minimizeMaxDistanceToGasStations.cpp
+++ BinarySearch/BS On Answers/minimizeMaxDistanceToGasStations.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+//Brute force approach
 double minMaxDist(vector<int> &stations, int k)
 {
     int n = stations.size();
@@ -31,6 +33,33 @@ double minMaxDist(vector<int> &stations, int k)
         maxAns = max(maxAns, sectionLength);
     }
     return maxAns;
+}
+*/
+
+// Better approach
+double minMaxDist(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    vector<int> howMany(n - 1, 0);
+    priority_queue<pair<long double, int>> pq;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        pq.push({arr[i + 1] - arr[i], i});
+    }
+
+    for (int gasStations = 1; gasStations <= k; gasStations++)
+    {
+        auto tp = pq.top();
+        pq.pop();
+        int secInd = tp.second;
+        howMany[secInd]++;
+
+        long double iniDiff = arr[secInd + 1] - arr[secInd];
+        long double newSecLen = iniDiff / (long double)(howMany[secInd] + 1);
+        pq.push({newSecLen, secInd});
+    }
+    return pq.top().first;
 }
 int main()
 {
