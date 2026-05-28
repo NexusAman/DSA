53b24165bd30d9a49243e597fe082761f7fc2dab
NexusAman <amankumarkeshri1@gmail.com>
2026-05-28 21:19:56 +0530

    solved findMatrixMedian using optimal approach

--- BinarySearch/BS On 2D Arrays/findMatrixMedian.cpp
+++ BinarySearch/BS On 2D Arrays/findMatrixMedian.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+// Brute force approach
 int findMedian(vector<vector<int>> &matrix)
 {
     int m = matrix.size();
@@ -21,6 +23,51 @@ int findMedian(vector<vector<int>> &matrix)
     int median = length / 2;
 
     return temp[median];
+}
+*/
+
+// Optimal approach
+int countSmallEquals(vector<vector<int>> &matrix, int x)
+{
+    int count = 0;
+    int m = matrix.size();
+
+    for (int i = 0; i < m; i++)
+    {
+        count += upper_bound(matrix[i].begin(), matrix[i].end(), x) - matrix[i].begin();
+    }
+    return count;
+}
+int findMedian(vector<vector<int>> &matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    int low = INT_MAX, high = INT_MIN;
+
+    for (int i = 0; i < m; i++)
+    {
+        low = min(matrix[i][0], low);
+        high = max(matrix[i][n - 1], high);
+    }
+
+    int req = (m * n) / 2;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        int smallEqual = countSmallEquals(matrix, mid);
+
+        if (smallEqual <= req)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return low;
 }
 int main()
 {
