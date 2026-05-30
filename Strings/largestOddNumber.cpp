95a9248a76dab6fda137e40abc53cb95632c6de1
NexusAman <amankumarkeshri1@gmail.com>
2026-05-30 18:48:39 +0530

    solved largestOddNumber problem using optimal approach

---
+++ Strings/largestOddNumber.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+string largestOddNumber(string num)
+{
+    int n = num.length();
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if ((num[i] - '0') % 2 != 0)
+        {
+            return num.substr(0, i + 1);
+        }
+    }
+    return "";
+}
+int main()
+{
+    string num = "35427";
+    string res = largestOddNumber(num);
+    cout << res;
+    return 0;
+}
