365a40ea0a1cd6783fe0e6630b82a0bab946300e
NexusAman <amankumarkeshri1@gmail.com>
2026-06-10 21:21:30 +0530

    .

---
+++ Strings/countSubstrings.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int countSubstrings(string s, int k)
+{
+    int n = s.length();
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            string substring = "";
+            for (int k = i; k <= j; k++)
+            {
+                substring += s[k];
+            }
+        }
+    }
+    return count;
+}
+int main()
+{
+    string s = "pqpqs";
+    int k = 2;
+    cout << countSubstrings(s, k);
+    return 0;
+}
