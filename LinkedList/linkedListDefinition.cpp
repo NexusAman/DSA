a1e8297196b218ccc991d8ca9708782d78e05718
NexusAman <amankumarkeshri1@gmail.com>
2026-06-18 18:39:17 +0530

    perform some basic linkedlist operations

--- LinkedList/linkedListDefinition.cpp
+++ LinkedList/linkedListDefinition.cpp
@@ -14,10 +14,8 @@ public:
     }
 };
 
-int main()
+ListNode *convertArrToLL(vector<int> &arr)
 {
-    // ListNode *node = new ListNode(10);
-    vector<int> arr = {10, 20, 30, 40};
     ListNode *head = new ListNode(arr[0]);
     ListNode *temp = head;
 
@@ -27,9 +25,62 @@ int main()
         temp = temp->next;
     }
 
-    // Print list
-    temp = head;
-    while (temp != nullptr)
+    return head;
+}
+
+int lengthOfLL(ListNode *head)
+{
+    int cnt = 0;
+    ListNode *temp = head;
+
+    while (temp)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+bool checkIfPresent(ListNode *head, int val)
+{
+    ListNode *temp = head;
+
+    while (temp)
+    {
+        if (temp->data == val)
+            return true;
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main()
+{
+    // ListNode *node = new ListNode(10);
+    vector<int> arr = {10, 20, 30, 40};
+
+    // Array to LL conversion
+    ListNode *head = convertArrToLL(arr);
+    ListNode *temp = head;
+
+    // Length of LL
+    int lengthLL = lengthOfLL(head);
+    cout << "Length of linkedlist : " << lengthLL << endl;
+
+    // Search element in LL
+    int val = 40;
+    bool isExist = checkIfPresent(head, val);
+    if (isExist)
+    {
+        cout << "Element exist in the LL" << endl;
+    }
+    else
+    {
+        cout << "Element doesn't exist in the LL" << endl;
+    }
+
+    // Print list (traverse)
+    while (temp)
     {
         cout << temp->data << " ";
         temp = temp->next;
