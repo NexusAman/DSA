d2391f4d4bed26f9fe9ebfd2cf7be2d9c7ea20eb
NexusAman <amankumarkeshri1@gmail.com>
2026-06-28 22:55:10 +0530

    solved removeNthNodeFromEnd using brute force approach

---
+++ LinkedList/removeNthNodeFromEnd.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+
+    ListNode(int x)
+    {
+        val = x;
+        next = nullptr;
+    }
+};
+
+ListNode *convertArrToLL(vector<int> &arr)
+{
+    ListNode *head = new ListNode(arr[0]);
+    ListNode *temp = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        temp->next = new ListNode(arr[i]);
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+void printList(ListNode *head)
+{
+    ListNode *temp = head;
+
+    while (temp)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+ListNode *removeNthFromEnd(ListNode *head, int n)
+{
+    ListNode *temp = head;
+    int cnt = 0;
+
+    while (temp)
+    {
+        cnt++;
+        temp = temp->next;
+    }
+
+    if (cnt == n)
+    {
+        ListNode *newHead = head->next;
+        delete head;
+        return newHead;
+    }
+
+    int res = cnt - n;
+    temp = head;
+
+    while (temp)
+    {
+        res--;
+        if (res == 0)
+        {
+            break;
+        }
+        temp = temp->next;
+    }
+
+    ListNode *deleteNode = temp->next;
+    temp->next = temp->next->next;
+    delete deleteNode;
+
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+
+    // Array to LL conversion
+    ListNode *head = convertArrToLL(arr);
+    ListNode *temp = head;
+    printList(head);
+
+    // remove nth node from end of the linked list
+    int n = 2;
+    head = removeNthFromEnd(head, n);
+    printList(head);
+    return 0;
+}
