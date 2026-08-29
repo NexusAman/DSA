040e247a81df2e1cc20f0515dcb2e58192519b31
NexusAman <amankumarkeshri1@gmail.com>
2026-08-29 10:06:11 +0530

    solved sumOfSubarrayMinimums using optimal approach

--- Stack & Queue/Monotonic Stack&Queue Problems/sumOfSubarrayMinimums.cpp
+++ Stack & Queue/Monotonic Stack&Queue Problems/sumOfSubarrayMinimums.cpp
@@ -24,27 +24,103 @@ using namespace std;
 // }
 
 // Better approach
-int sumSubarrayMins(vector<int> &arr)
+// int sumSubarrayMins(vector<int> &arr)
+// {
+//     long long ans = 0;
+//     int MOD = 1e9 + 7;
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         int currMin = INT_MAX;
+//         for (int j = i; j < arr.size(); j++)
+//         {
+//             currMin = min(currMin, arr[j]);
+
+//             ans = (ans + currMin) % MOD;
+//         }
+//     }
+//     return ans;
+// }
+
+vector<int> nextSmallerElement(vector<int> &arr)
 {
-    long long ans = 0;
-    int MOD = 1e9 + 7;
+    vector<int> ans(arr.size(), arr.size());
+    stack<int> st;
+
+    for (int i = arr.size() - 1; i >= 0; i--)
+    {
+        while (!st.empty() && arr[st.top()] >= arr[i])
+        {
+            st.pop();
+        }
+
+        if (!st.empty())
+        {
+            ans[i] = st.top();
+        }
+
+        st.push(i);
+    }
+
+    return ans;
+}
+
+vector<int> prevSmallerElement(vector<int> &arr)
+{
+    vector<int> ans(arr.size(), -1);
+    stack<int> st;
 
     for (int i = 0; i < arr.size(); i++)
     {
-        int currMin = INT_MAX;
-        for (int j = i; j < arr.size(); j++)
+        while (!st.empty() && arr[st.top()] > arr[i])
         {
-            currMin = min(currMin, arr[j]);
-
-            ans = (ans + currMin) % MOD;
+            st.pop();
         }
+
+        if (!st.empty())
+        {
+            ans[i] = st.top();
+        }
+        st.push(i);
     }
+    return ans;
+}
+
+int sumSubarrayMins(vector<int> &arr)
+{
+    vector<int> nse = nextSmallerElement(arr);
+    vector<int> pse = prevSmallerElement(arr);
+    int MOD = 1e9 + 7;
+    long long ans = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int leftChoices = i - pse[i];
+        int rightChoices = nse[i] - i;
+
+        long long totalChoices = 1LL * leftChoices * rightChoices;
+        long long contribution = 1LL * arr[i] * totalChoices;
+
+        ans = (ans + contribution) % MOD;
+    }
+
     return ans;
 }
 
 int main()
 {
     vector<int> arr = {3, 1, 2, 4};
+    vector<int> nse = nextSmallerElement(arr);
+    for (auto val : nse)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    vector<int> pse = prevSmallerElement(arr);
+    for (auto val : pse)
+    {
+        cout << val << " ";
+    }
     cout << sumSubarrayMins(arr);
     return 0;
 }
